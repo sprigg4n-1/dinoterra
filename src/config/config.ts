6531a428157dcc766cb2b51c2701845748c0b0d9
Roman <71217095+sprigg4n-1@users.noreply.github.com>
2025-02-27 22:12:38 +0100

    chore: change url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL_DINO = "http://localhost:8001/api/v1/dinosour";
-export const BASE_URL_DINO = "https://26.65.45.173:8001/api/v1/dinosour";
+export const BASE_URL_DINO = "http://localhost:8001/api/v1/dinosour";
+// export const BASE_URL_DINO = "https://26.65.45.173:8001/api/v1/dinosour";
