378397faecffa3f31d7c65605177acd24b564a0c
Roman <71217095+sprigg4n-1@users.noreply.github.com>
2025-02-28 11:40:04 +0100

    chore: change file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL_DINO = "http://localhost:8001/api/v1/dinosour";
 export const BASE_URL_DINO =
-  "https://dinosaur.railway.internal/api/v1/dinosour";
+  "https://dinosaur-production.up.railway.app/api/v1/dinosour";
